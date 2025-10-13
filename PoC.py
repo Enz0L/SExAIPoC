5c2c0f16a532b127fa2bacddc1f484c928427401
Enzo <60600449+Enz0L@users.noreply.github.com>
2025-10-13 22:49:55 +0200

    Update PoC.py

--- PoC.py
+++ PoC.py
@@ -2,6 +2,20 @@
 #Author: Enzo LE NAIR       #
 #Project Name: SExAI        #
 #############################
+#   Copyright (C) 2025  Enzo LE NAIR
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU Affero General Public License as
+#   published by the Free Software Foundation, either version 3 of the
+#   License, or (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU Affero General Public License for more details.
+#
+#   You should have received a copy of the GNU Affero General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import fofa
 import requests
